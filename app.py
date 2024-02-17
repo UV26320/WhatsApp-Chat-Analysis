859fa1a1f4269405a0b94ff7f666a0d732fe2f64
UDAY VARA <udayvara26@gmail.com>
2024-02-17 20:02:35 +0530

    uplode file funcationality Added

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.sidebar.title("WhatsApp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
