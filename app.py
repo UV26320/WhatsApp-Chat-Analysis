d113a54b0b683db956e13bbd99adb2d273bae20c
UDAY VARA <udayvara26@gmail.com>
2024-02-18 12:33:02 +0530

    stream data -> string data

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import preprocessor
 
 st.sidebar.title("WhatsApp Chat Analyzer")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
+    # now we can convert stream data to string data
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
